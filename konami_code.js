4bcd2a59b14f0edeccd5c2c5c6442b8f8a5dbd30
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 05:55:56 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,17 +13,7 @@ const codes = [
 
 var index = 0
 function init() {
-  document.body.addEventListener('keydown', (e) => {
-    const key = e.key
-    
-    if (key === codes[index]) {
-      index++
-      if (index == codes.length) {
-        alert("Success!")
-        index = 0
-      }
-    } else {index = 0}
-  })
+  document.body.addEventListener('keydown', (e) => {})
 }
 
 init()
